4fd2304a646898c1a70bdc202c3b5252b0233a31
Abigail-cloud <toluabby12@gmail.com>
2024-04-15 19:29:06 -0700

    2-is_same_class.py'

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+'''Task 02 - 2. Exact same object'''
+
+
+def is_same_class(obj, a_class):
+    '''function that returns True if the object is exactly
+    an instance of the specified class ; otherwise False.
+    it is a subclass to the parent class'''
+    return type(obj) is a_class
