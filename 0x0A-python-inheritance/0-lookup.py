e5502ad52e68ef7dd6f72d5caf5958d3faf0ca58
Abigail-cloud <toluabby12@gmail.com>
2024-04-15 19:22:47 -0700

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+'''Task 00 - 0. Lookup inheritance'''
+
+
+def lookup(obj):
+    '''Function that returns the list of available properties
+    and methods of an object'''
+    return dir(obj)
