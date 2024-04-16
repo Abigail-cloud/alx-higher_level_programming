91ed6af5b90edf421cfe5e826ab08e6e262ac330
Abigail-cloud <toluabby12@gmail.com>
2024-04-15 19:58:42 -0700

    10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,14 +1,17 @@
-
-'''Task 10 - 10. Square #1'''
-
-
+#!/usr/bin/python3
+"""Defines a Rectangle subclass Square."""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    '''class Square : rectangle as parameter'''
+    """Represent a square."""
+
     def __init__(self, size):
-        '''Initializing atributes'''
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
-        super().__init__(self.__size, self.__size)
