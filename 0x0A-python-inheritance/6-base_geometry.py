36001026021f4ff7c119103934ccd4b627d799e6
Abigail-cloud <toluabby12@gmail.com>
2024-04-15 19:45:15 -0700

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+'''Task 06 - 6. Improve Geometry: more on the base geometry'''
+
+
+class BaseGeometry():
+    '''class BaseGeometry added method'''
+    def area(self):
+        '''Function that raises an Exception with the message area()
+        is not implemented'''
+        raise Exception("area() is not implemented")
